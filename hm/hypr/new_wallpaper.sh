02e238d2f5c4dab97220ca96c9cd185a5b28b2ad
eggy <eastjayhawk@gmail.com>
2024-09-15 16:13:02 -0400

    moved a bunch of dotfiles into home-manager folder this way i can import them via hm on my laptop

---
+++ hm/hypr/new_wallpaper.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#Modified version of the randomize script by swww to not require passing an input
+
+
+DEFAULT_DIR="/home/eggy/.config/wallpapers"
+DIR="${1:-$DEFAULT_DIR}"
+if [[ ! -d $DIR ]]; then
+	echo "Directory not found: $DIR"
+	exit 1
+fi
+
+export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION_STEP=2
+
+INTERVAL=300
+
+while true; do
+    find "$DIR" -type f \
+	    | while read -r img; do
+    		echo "$((RANDOM % 1000)):$img"
+	    done \
+	    | sort -n | cut -d':' -f2- \
+	    | while read -r img; do
+	        swww img "$img"
+		sleep $INTERVAL
+	done
+done
---
+++ hm/hypr/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+waybar &
+
+#init the wallpaper daemon
+#SWWW-daemon  & 
+# setting the wallpaper
+# SWWW img ~/.config/wallpapers/test.png &
+
+nm-applet --indicator &
+
+#waybar &
+
+dunst 
+
+
