49a6a8d7452dc205346227286b714c43099cf949
eggy <eggy@flopwampa.com>
2024-09-25 07:31:22 -0400

    set up steam.nix to finally use the mkEnable!!

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,29 +1,36 @@
 #steam.nix
 #opengl, and maybe also proton stuff
 
- { pkgs, ... }:
+ { pkgs, lib, config, ... }:
 
  {
-   hardware.graphics = {
-     enable = true;
-  #   driSupport = true;
-     enable32Bit = true;
+   options = {
+     steam.enable = 
+       lib.mkEnableOption "enables steam";
    };
 
-   programs.steam.enable = true;
-   programs.steam.gamescopeSession.enable = true;
+   config = lib.mkIf config.steam.enable {
 
-   environment.systemPackages = with pkgs; [
-     mangohud
-   ];
+     hardware.graphics = {
+       enable = true;
+  #    driSupport = true;
+       enable32Bit = true;
+     };
 
-   programs.gamemode.enable = true;
+     programs.steam.enable = true;
+     programs.steam.gamescopeSession.enable = true;
+
+     environment.systemPackages = with pkgs; [
+       mangohud
+     ];
+
+     programs.gamemode.enable = true;
    
-   environment.sessionVariables = {
-     STEAM_EXTRA_COMPAT_TOOLS_PATH = 
-     "/home/eggy/.stean/root/compatibilitytools.d";
+     environment.sessionVariables = {
+       STEAM_EXTRA_COMPAT_TOOLS_PATH = 
+       "/home/eggy/.stean/root/compatibilitytools.d";
+     };
    };
-
   }
 
 
