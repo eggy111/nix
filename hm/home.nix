984b04245e162a92e34b4986e816a1e5d329dd36
eggy <eggy@flopwampa.com>
2024-10-18 17:10:07 -0400

    moved pyprland config into repo, will eventually use home.file.path.text and just move it into a pyprland.nix file as well, but for now

--- hm/home.nix
+++ hm/home.nix
@@ -51,48 +51,8 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    #hyprland config files
-#    ".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
-#    ".config/hypr/hypridle.conf".source = ./hypr/hypridle.conf;
-#    ".config/hypr/colors.conf".source = ./hypr/colors.conf;
-#    ".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
-#    ".config/hypr/new_wallpaper.sh".source = ./hypr/new_wallpaper.sh;
-#    ".config/hypr/start.sh".source = ./hypr/start.sh;
-#    ".config/hypr/wallpaper.sh".source = ./hypr/wallpaper.sh;
+     ".config/hypr/pyprland.toml".source = ./pyprland.toml;
 
-    #wallpapers im sure there is a better way to do this.... but im lazy r
-#    ".config/wallpapers/1.png".source = ./wallpapers/1.png;
-#    ".config/wallpapers/2.png".source = ./wallpapers/2.png;
-#    ".config/wallpapers/3.png".source = ./wallpapers/3.png;
-#    ".config/wallpapers/4.png".source = ./wallpapers/4.png;
-#    ".config/wallpapers/5.png".source = ./wallpapers/5.png;
-#    ".config/wallpapers/6.png".source = ./wallpapers/6.png;
-#    ".config/wallpapers/7.png".source = ./wallpapers/7.png;
-#    ".config/wallpapers/8.png".source = ./wallpapers/8.png;
-#    ".config/wallpapers/9.png".source = ./wallpapers/9.png;
-#    ".config/wallpapers/10.png".source = ./wallpapers/10.png;
-#    ".config/wallpapers/11.png".source = ./wallpapers/11.png;
-#    ".config/wallpapers/12.png".source = ./wallpapers/12.png;
-#    ".config/wallpapers/13.png".source = ./wallpapers/13.png;
-#    ".config/wallpapers/14.png".source = ./wallpapers/14.png;
-#    ".config/wallpapers/15.png".source = ./wallpapers/15.png;
-#    ".config/wallpapers/16.png".source = ./wallpapers/16.png;
-#    ".config/wallpapers/17.png".source = ./wallpapers/17.png;
-#    ".config/wallpapers/18.png".source = ./wallpapers/18.png;
-#    ".config/wallpapers/19.png".source = ./wallpapers/19.png;
-#    ".config/wallpapers/20.png".source = ./wallpapers/20.png;
-#    ".config/wallpapers/21.png".source = ./wallpapers/21.png;
-#    ".config/wallpapers/22.png".source = ./wallpapers/22.png;
-#    ".config/wallpapers/23.png".source = ./wallpapers/23.png;
-#    ".config/wallpapers/24.png".source = ./wallpapers/24.png;
-#    ".config/wallpapers/A.png".source = ./wallpapers/A.png;
-#    ".config/wallpapers/B.png".source = ./wallpapers/B.png;
-#    ".config/wallpapers/C.png".source = ./wallpapers/C.png;
-#    ".config/wallpapers/jerma_nichijou.gif".source = ./wallpapers/jerma_nichijou.gif;
-
- #   #waybar config
- #   ".config/waybar/config".source = ./waybar/config;
- #   ".config/waybar/style.css".source = ./waybar/style.css;
     
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
