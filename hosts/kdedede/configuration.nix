b302179cc8d338d4cd0b0a52142227f4fdfd20f2
eggy <eggy@flopwampa.com>
2025-07-06 16:59:37 -0400

    hostid

--- hosts/kdedede/configuration.nix
+++ hosts/kdedede/configuration.nix
@@ -20,6 +20,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "kdedede"; # Define your hostname.
+  networking.hostId = "1585cc02";
 
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
