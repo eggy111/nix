d9fef48bee346971ac179a68fded7a2dabf3846e
eggy <eggy@flopwampa.com>
2025-10-24 22:56:10 -0400

    updated stuff methinks

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,16 +1,21 @@
 # fonts.nix
 # configuration file to load fonts
 
-{  pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 
 {
 
   environment.systemPackages = with pkgs; [
     liberation_ttf_v1
+    akkadian
     # nerdfonts
 
   ];
 
-  fonts.packages = (lib.filter lib.isDerivation (lib.attrValues pkgs.nerd-fonts)) ++ [ pkgs.liberation_ttf_v1 ];
+  fonts.packages = (lib.filter lib.isDerivation (lib.attrValues pkgs.nerd-fonts)) ++ [
+    pkgs.liberation_ttf_v1
+    pkgs.noto-fonts
+    pkgs.noto-fonts-cuneiform
+  ];
 
 }
