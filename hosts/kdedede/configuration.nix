90254e4983ec96dec0c8b5b106057a5c8fa66c3f
eggy <eastjayhawk@gmail.com>
2025-11-29 12:15:03 -0500

    look, idk if this will work but in theory this should give me access to my raid array lol

--- hosts/kdedede/configuration.nix
+++ hosts/kdedede/configuration.nix
@@ -25,48 +25,20 @@
   networking.hostName = "kdedede"; # Define your hostname.
   networking.hostId = "1585cc02";
 
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable CUPS to print documents.
+  programs.fish.enable = true;
+  programs.firefox.enable = false;
+  services.openssh.enable = true;
   services.printing.enable = false;
 
-  # Enable zsh
-  #  programs.zsh.enable = true;
-  programs.fish.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Install firefox.
-  programs.firefox.enable = false;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.activatedScripts.vaultPermissions = {
+    text = ''
+      chown -R eggy:users /vault
+      chmos -R 775 /vault
+    '';
+  };
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
