6cc0dbd661e9a59dda50777b2cbc44942e2cdbcd
eggy <eastjayhawk@gmail.com>
2026-03-10 22:16:08 -0400

    typo

--- flake.nix
+++ flake.nix
@@ -45,10 +45,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     polymc.url = "github:PolyMC/PolyMC";
-  };
-  zen-browser = {
-    url = "github:youwen5:zen-browser-flake";
-    inputs.nixpkgs.follows = "nixpkgs";
+
+    zen-browser = {
+      url = "github:youwen5:zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
